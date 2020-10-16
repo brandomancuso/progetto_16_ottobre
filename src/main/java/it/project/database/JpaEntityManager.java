efa740ba24e5f035369fa12aacf11951e494aa34
Padawan10 <Padawan10@LAPTOP-G8D3LA7T.homenet.telecomitalia.it>
2020-10-16 12:33:27 +0200

    modifiche funzionali

--- src/main/java/it/project/database/JpaEntityManager.java
+++ src/main/java/it/project/database/JpaEntityManager.java
@@ -2,7 +2,6 @@ package it.project.database;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import org.apache.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Component;
 public class JpaEntityManager {
 
 	private static EntityManagerFactory factory;
-	private static  Logger log = Logger.getLogger(JpaEntityManager.class);
+	
 	
 	private JpaEntityManager() {} // makes constructor unaccessible
 	
@@ -19,7 +18,7 @@ public class JpaEntityManager {
 	public static EntityManagerFactory getInstance() {
 		if(factory == null) {
 			factory = Persistence.createEntityManagerFactory("OortActivity");
-			log.debug("Entity manager factory created.");
+			System.out.println("Entity manager factory created.");
 		}
 		return factory;
 	}
