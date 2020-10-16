860d8582f0989b0d64a2643f32470e6db21afcb1
Padawan10 <Padawan10@LAPTOP-G8D3LA7T.homenet.telecomitalia.it>
2020-10-16 11:07:28 +0200

    controller login

---
+++ src/main/java/it/project/controller/LoginController.java
@@ -0,0 +1,15 @@
+package it.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	
+	@RequestMapping(value = "/redirect", method = RequestMethod.GET)
+	public String conferma() {
+		System.out.println("Redirect..");
+		return "conferma";
+	}
+}
